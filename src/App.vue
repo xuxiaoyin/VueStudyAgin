e6e7cb0cd2e0df90be41e22d395a18bf34569989
xuxiaoyin <279238850@qq.com>
2019-10-30 23:27:30 +0800

    vuex源码实现

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,32 @@
   <div id="app">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> | 
-    <router-link to="/login" v-if="!$store.state.isLogin">Login</router-link>
+    <!-- <router-link to="/login" v-if="!$store.state.isLogin">Login</router-link> -->
+    <router-link to="/login" v-if="!isLogin">Login</router-link>
     <span v-else>欢迎回来</span>
+    <div>{{count}}</div>
+    <button @click="onAdd">add</button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import store from './KStore'
 
 export default {
-  name: 'app'
+  name: 'app',
+  computed: {
+    count() {
+      return store.state.count
+    },
+    ...mapState(['isLogin'])
+  },
+  methods: {
+    onAdd() {
+      store.commit('add')
+    }
+  },
 }
 </script>
 
---
+++ src/KStore.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+
+class KStore {
+  constructor(options) {
+    this.state = options.state
+    this.mutations = options.mutations
+    this.actions = options.actions
+    this.vm = new Vue({
+      data: {
+        state: this.state
+      }
+    })
+  }
+ 
+  commit(type, payload) {
+    const mutation = this.mutations[type]
+    mutation(this.state, payload)
+  }
+
+  dispatch(type, payload) {
+    const action = this.actions[type]
+    const ctx = {
+      commit: this.commit,
+      state: this.state
+    }
+    return action(ctx, payload)
+  }
+}
+
+export default new KStore({
+  state: {count: 1},
+  mutations: {
+    add(state) {
+      state.count ++ 
+    }
+  }
+})
